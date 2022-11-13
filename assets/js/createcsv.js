088c8013a429d98834611e9d28623fb350e6bf6c
allochthonous <allochthonous@users.noreply.github.com>
2022-11-12 21:54:27 -0500

    Initial list of Earthsci people

--- assets/js/createcsv.js
+++ assets/js/createcsv.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
   console.log( "ready!" );
   $.ajax({
     type: "GET",
-    url: "resources/hpsers.csv",
+    url: "resources/earthodons.csv",
     dataType: "text",
     success: function(data) {
       printSociologistsOnWP(data); //define your own function
